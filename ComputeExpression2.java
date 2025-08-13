21b7f40d0b1efbfcb74d2815c6056450fd22f9e2
24ftt1827 <24FTT1827@student.pb.edu.bn>
2025-08-13 18:50:40 +0800

    Add files via upload

---
+++ ComputeExpression2.java
@@ -0,0 +1,8 @@
+public class ComputeExpression2 {
+    public static void main(String[] args) {
+        System.out.println("7.5x2.5-1.5x3\n" +
+                           "------------------\n" +
+                           "4.5-2.5");
+                            System.out.println("= 9.5");}
+    }
+
---
+++ DisplayPattern.java
@@ -0,0 +1,8 @@
+public class DisplayPattern {
+    public static void main(String[] args) {
+        System.out.println("(\\__/) I AM AN");
+        System.out.println("( @W@)          OOO  W       W L");
+        System.out.println("/) W )         O   O  W  W  W  L");
+        System.out.println(" ^  ^           OOO     W W    L L L");
+    }
+}
