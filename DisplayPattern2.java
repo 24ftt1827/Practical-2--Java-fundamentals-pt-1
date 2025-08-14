2528a68beadb7acb93feb985d341661ecb8a400f
24ftt1827 <24FTT1827@student.pb.edu.bn>
2025-08-14 10:18:25 +0800

    Update DisplayPattern2.java

--- DisplayPattern2.java
+++ DisplayPattern2.java
@@ -1,6 +1,6 @@
 public class DisplayPattern2 {
     public static void main(String[] args) {
-        System.out.println(".       .=\"\"=.");
+        System.out.println(".     .=\"\"=.");
         System.out.println("    /       \\");
         System.out.println("   |  O    O | ");
         System.out.println("   \\    /\\   /");
